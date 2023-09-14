eb43966c75c0a4172b0e68575e7fac26e5f6289c
BrunoMSPais <bpais@outlook.pt>
2023-09-14 13:03:11 +0100

    implementing cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { uploadVideoRoute } from './routes/upload-video'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*', // TODO: - replace with frontend URL in production
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
