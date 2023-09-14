4d74edc74292b57d7e210ac7fbb9685ae70dcb26
BrunoMSPais <bpais@outlook.pt>
2023-09-14 12:59:41 +0100

    generate ai completion route

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from 'fastify'
 import { uploadVideoRoute } from './routes/upload-video'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333
